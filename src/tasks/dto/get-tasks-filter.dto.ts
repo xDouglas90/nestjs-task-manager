3aa5bf7240ed41556e0ca0b8694117e5a4732adc
xDouglas90 <odouglas.dev@gmail.com>
2023-02-18 22:38:27 -0300

    refactor: remove task model and create task status enum :recycle:

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,4 +1,4 @@
-import { TaskStatus } from '../tasks.model';
+import { TaskStatus } from '../task-status.enum';
 import { IsOptional, IsIn, IsNotEmpty, IsString } from 'class-validator';
 
 export class GetTasksFilterDto {
