b858cb1cb384d55fcdae6465476b68fdc35ed025
xDouglas90 <odouglas.dev@gmail.com>
2023-02-20 04:29:55 -0300

    refactor: add JwtModule and AuthModule :recycle:

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthModule } from 'src/auth/auth.module';
+import { jwtConstants } from 'src/auth/constants';
 import { databaseProvider } from 'src/core/database/database.provider';
 import { taskRepository } from './providers/task-repository.provider';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 
 @Module({
+  imports: [
+    AuthModule,
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: jwtConstants.secret,
+        signOptions: { expiresIn: jwtConstants.expiresIn },
+      }),
+    }),
+  ],
   controllers: [TasksController],
   providers: [...databaseProvider, ...taskRepository, TasksService],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,8 +3,20 @@ import { databaseProvider } from 'src/core/database/database.provider';
 import { userRepository } from './providers/user-repository.provider';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { AuthModule } from 'src/auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from 'src/auth/constants';
 
 @Module({
+  imports: [
+    AuthModule,
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: jwtConstants.secret,
+        signOptions: { expiresIn: jwtConstants.expiresIn },
+      }),
+    }),
+  ],
   controllers: [UsersController],
   providers: [...databaseProvider, ...userRepository, UsersService],
   exports: [UsersService],
